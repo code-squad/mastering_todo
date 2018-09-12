c50b4feb69072962979de1e9a023b01ae3c2c849
Nam Won Hyung <nwh0526@gmail.com>
2018-09-12 20:09:55 +0900

    2주차 todoList 과제 제출합니다. (#3)
    * commit todoList.js command 함수 설계 중간 commit
    
    * update todoList.js - todoList 함수 설계 저장. 함수 기능 미구현 상태
    
    * update todoList.js - 일정 add 기능 구현. 중복제거 필요
    
    * update todoList.js - 할일 상태 update 기능 구현
    
    * update todoList.js - 일정 show 기능 구현
    
    * update todoList.js - 리팩토링하면서 executeTodoList is not a function 에러발생.. 시간상 나중에 수정.. done 상태 업데이트시 시간 출력되도록 개발 필요
    
    * update codesquad FP 과제 1차 완성. done 상태로 update될 경우 시간차 나오는 로직은 미구현
    
    * update todoList.js - piepline 사용 함수 변경 및 관련 있는 함수끼리 모아 놓음
    
    * update todoList.js - add validation function
    
    * update todoList.js - 관련 있는 function끼리 모아 놓음

---
+++ task.js
@@ -0,0 +1,15 @@
+module.exports = {
+    SEPARATOR : "$",
+    INDEX: {
+        COMMAND: 0,
+        TASKID: 1,
+        REMARK: 1,
+        SHOW: 1,
+        UPDATE: 2,
+    },
+    STATUS: {
+        TODO: 0,
+        DOING: 1,
+        DONE: 2
+    }
+};
---
+++ todoList.js
@@ -0,0 +1,133 @@
+const TASK = require('./task.js');
+
+const pipeline = (...functions) => args => functions.reduce((arg, nextFn) => nextFn(arg), args);
+
+
+const split = (regExp) => (str) => str.split(regExp);
+const toLowerCase = (str) => str.toLowerCase();
+const trim = (str) => str.trim();
+
+const taskMap = new Map();
+const executeCommand = (commandList) => {
+  const command = extractCommand(commandList);
+  
+  if(command === 'add') addTask(taskMap, commandList);
+  else if(command === 'update') updateStatus(taskMap, commandList);
+  else if(command === 'show') showSelectTask(taskMap, commandList);
+  else console.log('명령어를 잘못 입력하셨습니다.');
+}
+
+const command = pipeline(
+  split(TASK.SEPARATOR),
+  executeCommand
+);
+
+const getCommand = (index) => (arr) => arr[index];
+const extractCommand = pipeline(
+  getCommand(TASK.INDEX.COMMAND),
+  trim,
+  toLowerCase
+)
+
+const addTask = (taskMap, commandList) => {
+  const sequence = taskMap.size + 1;
+  const taskObj = {
+    taskId: sequence, 
+    remark: commandList[TASK.INDEX.REMARK], 
+    status: TASK.STATUS.TODO,
+    rgstDate: new Date()
+  };
+  taskMap.set(sequence, taskObj);
+  
+  showCurrentTaskList(taskMap);
+}
+
+const updateStatus = (taskMap, commandList) => {
+  try {
+    const taskId = extractTaskId(commandList);
+    if(!taskMap.has(taskId)) return;
+
+    const status = getTaskStatus(commandList[TASK.INDEX.UPDATE]);
+    const taskObj = taskMap.get(taskId);
+    const newTaskObj = Object.assign({}, taskObj, {
+      status: status, 
+      updtDate: new Date()
+    });
+    taskMap.set(taskId, newTaskObj);
+  
+    showCurrentTaskList(taskMap);
+  } catch (error) {
+  }
+}
+
+const getTaskId = (arr) => arr[TASK.INDEX.TASKID];
+const validateTaskId = (taskId) => {
+  if(!/^[0-9]+$/.test(taskId)) {
+    console.log("잘못된 일정 ID 값을 입력하였습니다.");
+    throw new Error();
+  }
+  return taskId;
+}
+const extractTaskId = pipeline(
+  getTaskId,
+  validateTaskId,
+  Number
+);
+
+const validateStatus = (status) => {
+  if(!/^((todo)|(doing)|(done))$/i.test(status)) {
+    console.log("입력한 상태값이 올바르지 않습니다.");
+    throw new Error();
+  }
+  return status;
+}
+
+const matchStatus = (status) => {
+  if(status === "todo") return TASK.STATUS.TODO;
+  else if(status === "doing") return TASK.STATUS.DOING;
+  else if(status === "done") return TASK.STATUS.DONE;
+  else return -1;
+}
+
+const getTaskStatus = pipeline(
+  trim,
+  validateStatus,
+  matchStatus,
+  Number,
+);
+
+const showSelectTask = (taskMap, commandList) => {
+  if(taskMap.size === 0) {
+    console.log("상태 조회 결과가 없습니다.");
+    return;
+  }
+  try {
+    const status = getTaskStatus(commandList[TASK.INDEX.SHOW]);
+    let resultList = [];
+    for(let [key, value] of taskMap) {
+      if(value.status === status)
+        resultList = [` ${value.taskId}, ${value.remark}`, ...resultList];
+    }
+    console.log(resultList.toString());
+  } catch (error) {
+  }
+}
+
+const showCurrentTaskList = (taskMap) => {
+  const taskCountList = [0, 0, 0];
+  for(let [, value] of taskMap) {
+    taskCountList[value.status]++;
+  }
+
+  console.log(`현재상태 : todo:${taskCountList[TASK.STATUS.TODO]}개,`
+                      + ` doing:${taskCountList[TASK.STATUS.DOING]}개,` 
+                      + ` done:${taskCountList[TASK.STATUS.DONE]}개`);
+}
+
+command('aDD   $codesquad javascript!!');
+command('aDD   $codesquad javascript!!');
+command('aDD   $codesquad javascript!!');
+command('update   $1$aasdfdoing');
+command('update   $2$done');
+command('update   $3$doing');
+command('show   $doing');
