4c84cfdf2cdf626798750584c01c75de120c4bfe
jjori.master <jjori.master@gmail.com>
2018-09-19 00:12:22 +0900

    todo list 구현   - 상태 변경시 상태 변경시점 기록   - 상태 변경시 doing을 거치지 않고 done으로 변경시 doing시간을 todo시간과 같게 설정   - doing 부터 done까지 걸린 시간 반환 함수 구현
      - command 함수
       = fns 함수 모음 객체에서 함수 이름이 key가 되도록 설정
       = 명령어 파서를 통해 제일 첫째 인자가 fns 객체에서 함수를 가져갈 수 있도록 설정
       = javascript 함수 인자를 받지 않으면 그냥 무시된다는 특성을 이용한 함수 호출후 인자를 일괄 적용

--- todo.js
+++ todo.js
@@ -10,46 +10,128 @@ const generateId = () => {
     return ids.push(null);
 };
 
-function Task(status, contents) {
+function Task(id, status, contents) {
+    this.id = id;
     this.status = status;
     this.contents = contents;
-    const time = {
-        todo: new Date(),
-        doing: null,
-        done: null
+
+    this.time = {
+        'todo': new Date(),
+        'doing': null,
+        'done': null,
+    };
+
+    this.getTookDateTime = () => {
+        if(this.status !== DONE) {
+            throw '아직 종료되지 않은 작업니다.';
+        }
+
+        const timeDiff = this.time.done - this.time.doing;
+
+        return [
+            { dateTime: Math.floor(timeDiff / 1000 / 60 / 60 / 24), divDesc: '일'},
+            { dateTime: Math.floor(timeDiff / 1000 / 60 / 60 % 24), divDesc: '시간'},
+            { dateTime: Math.floor(timeDiff / 1000 / 60 % 60), divDesc: '분'},
+            { dateTime: Math.floor(timeDiff / 1000 % 60), divDesc: '초'}
+        ].filter((diffTime) => {
+            return diffTime.dateTime > 0;
+        }).map((diffTime) => {
+            return diffTime.dateTime + diffTime.divDesc;
+        }).join(' ') || '순삭!!';
+    };
+
+    this.changeStatus = (status) => {
+        if(!this.isValidTodoStatus(status)) {
+            throw status + '는 유요한 Todo List 상태 정보가 아닙니다.!!';
+        }
+
+        // doing status없이 done상태로 벼경시 doing time은 todo time으로 변경
+        if(status === DONE && !this.time.doing) {
+            this.time.doing = this.time.todo;
+        }
+
+        this.status = status;
+        this.time[status] = new Date();
+    };
+
+    this.isValidTodoStatus = (status) => {
+        return [TODO, DOING, DONE].includes(status);
     };
 };
 
 const command = (commandStr) => {
     const printNowStatus = () => {
         const status = tasks.reduce((sum, cur) => {
-            sum[cur.task.status]++;
+            sum[cur.status]++;
             return sum;
         }, {
-            todo: 0,
-            doing: 0,
-            done: 0
-        });
+                todo: 0,
+                doing: 0,
+                done: 0
+            });
 
         console.log('현재 상태: todo: ', status.todo, ', doing: ', status.doing, ', done: ', status.done, '\n');
     };
 
-    const [method, param1, param2] = commandStr.replace(/\s/g,'').toLowerCase().split('$');
-    switch(method) {
-        case 'add':
+    const abc = {
+        'add': (contents) => {
             const id = generateId();
-            tasks.push({
-                id: id,
-                task: new Task(TODO, param1)
-            });
-            
+            tasks.push(new Task(id, TODO, contents));
+
             console.log('id:', id, '"' + param1 + '" 항목이 새로 추가됐습니다.');
             printNowStatus();
-        break;
+        },
+
+        'update': (id, status) => {
+            const targetTask = tasks.filter((task) => {
+                return task.id === Number(id);
+            })[0];
+
+            targetTask.changeStatus(status);
+            printNowStatus();
+        },
+
+        'show': (status) => {
+            const targetTasks = tasks.filter((task) => {
+                return task.status === status;
+            }).reduce((results, task) => {
+                const ccc = [task.id, task.contents];
+                if(status === DONE) {
+                    ccc.push(task.getTookDateTime());
+                }
+
+                results.push(ccc.join(', '));
+                return results;
+            }, []).join(', ');
+
+            console.log(targetTasks + '\n');
+        }
     }
+
+    const [method, param1, param2] = commandStr.replace(/\s/g, '').toLowerCase().split('$');
+
+    abc[method](param1, param2);
 };
 
 
 command('Add $냐옹이는 냐옹냐옹');
 command('A dD $ 헬로헬로!!');
+
+command("show$todo");
+
+command('update$1$doing');
+
+command("show$doing");
+
+command("update$1$done");
+command("update$2$done");
+    
+// 걸린 시간 테스트를 위한 1일 1시간 30분 27초+
+tasks[0].time.done = new Date(new Date().getTime() +(1000 * 27) + (1000 * 60 * 90) + (1000 * 60 * 60 * 24));
+// 27분 10초
+tasks[1].time.done = new Date(new Date().getTime() + (1000 * 10) + (1000 * 60 * 27));
+
+command("show$done");
+
+
 
