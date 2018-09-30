38a2f646e6cc268c25ef0ad4f6c32bc04ef8483c
jjori.master <jjori.master@gmail.com>
2018-09-30 23:29:11 +0900

    할일 생성 방식 변경  - 기존 new를 통한 클래스 생성    이후 객체 리터럴를 통한 할일생성 및 등록 - 객체 안에 존재하는 함수를 외부 영역으로 이동

--- todo.js
+++ todo.js
@@ -4,100 +4,90 @@ const TODO = 'todo';
 const DOING = 'doing';
 const DONE = 'done';
 
-const tasks = [];
-const ids = [];
+let tasks = [];
+
+let id = 1;
 const generateId = () => {
-    return ids.push(null);
+    return id++;
 };
 
-function Task(id, status, contents) {
-    this.id = id;
-    this.status = status;
-    this.contents = contents;
+// 시간이 얼마나 지났는지 구하기
+const getTookDateTime = (startDateTime, endDateTime) => {
+    const dateTimeDiff = endDateTime - startDateTime;
+    
+    const tookDateTime = [
+        { dateTime: Math.floor(dateTimeDiff / 1000 / 60 / 60 / 24), divDesc: '일'},
+        { dateTime: Math.floor(dateTimeDiff / 1000 / 60 / 60 % 24), divDesc: '시간'},
+        { dateTime: Math.floor(dateTimeDiff / 1000 / 60 % 60), divDesc: '분'},
+        { dateTime: Math.floor(dateTimeDiff / 1000 % 60), divDesc: '초'}
+    ].filter((diffDateTime) => {
+        return diffDateTime.dateTime > 0;
+    }).map((diffDateTime) => {
+        return diffDateTime.dateTime + diffDateTime.divDesc;
+    }).join(' ') || '순삭!!';
 
-    this.time = {
-        'todo': new Date(),
-        'doing': null,
-        'done': null,
-    };
+    return tookDateTime;
+};
 
-    this.getTookDateTime = () => {
-        if(this.status !== DONE) {
-            throw '아직 종료되지 않은 작업니다.';
-        }
+// 현재 상태 정보 확인
+const printNowStatus = (tasks) => {
+    const status = tasks.reduce((sum, cur) => {
+        sum[cur.status]++;
+        return sum;
+    }, {
+            todo: 0,
+            doing: 0,
+            done: 0
+        });
 
-        const timeDiff = this.time.done - this.time.doing;
-
-        return [
-            { dateTime: Math.floor(timeDiff / 1000 / 60 / 60 / 24), divDesc: '일'},
-            { dateTime: Math.floor(timeDiff / 1000 / 60 / 60 % 24), divDesc: '시간'},
-            { dateTime: Math.floor(timeDiff / 1000 / 60 % 60), divDesc: '분'},
-            { dateTime: Math.floor(timeDiff / 1000 % 60), divDesc: '초'}
-        ].filter((diffTime) => {
-            return diffTime.dateTime > 0;
-        }).map((diffTime) => {
-            return diffTime.dateTime + diffTime.divDesc;
-        }).join(' ') || '순삭!!';
-    };
-
-    this.changeStatus = (status) => {
-        if(!this.isValidTodoStatus(status)) {
-            throw status + '는 유요한 Todo List 상태 정보가 아닙니다.!!';
-        }
-
-        // doing status없이 done상태로 벼경시 doing time은 todo time으로 변경
-        if(status === DONE && !this.time.doing) {
-            this.time.doing = this.time.todo;
-        }
-
-        this.status = status;
-        this.time[status] = new Date();
-    };
-
-    this.isValidTodoStatus = (status) => {
-        return [TODO, DOING, DONE].includes(status);
-    };
+    console.log('현재 상태: todo: ', status.todo, ', doing: ', status.doing, ', done: ', status.done, '\n');
 };
 
 const command = (commandStr) => {
-    const printNowStatus = () => {
-        const status = tasks.reduce((sum, cur) => {
-            sum[cur.status]++;
-            return sum;
-        }, {
-                todo: 0,
-                doing: 0,
-                done: 0
+    const fns = {
+        'add': (tasks, contents) => {
+            tasks.push({
+                id: generateId(),
+                status: TODO,
+                contents: contents,
+                time: {
+                    todo: new Date(),
+                    doing: null,
+                    done: null
+                }
             });
 
-        console.log('현재 상태: todo: ', status.todo, ', doing: ', status.doing, ', done: ', status.done, '\n');
-    };
-
-    const fns = {
-        'add': (contents) => {
-            const id = generateId();
-            tasks.push(new Task(id, TODO, contents));
-
             console.log('id:', id, '"' + param1 + '" 항목이 새로 추가됐습니다.');
-            printNowStatus();
+            printNowStatus(tasks);
+
+            return tasks;
         },
 
-        'update': (id, status) => {
+        'update': (tasks, id, status) => {
             const targetTask = tasks.filter((task) => {
                 return task.id === Number(id);
             })[0];
 
-            targetTask.changeStatus(status);
-            printNowStatus();
+            targetTask.status = status;
+            targetTask.time[status] = new Date();
+
+            if(status === DONE 
+                && targetTask.time.doing === null) {
+                    targetTask.time.doing = targetTask.time.todo;
+                }
+
+            printNowStatus(tasks);
+
+            return tasks;
         },
 
-        'show': (status) => {
+        'show': (tasks, status) => {
             const targetTasks = tasks.filter((task) => {
                 return task.status === status;
             }).reduce((results, task) => {
                 const ccc = [task.id, task.contents];
                 if(status === DONE) {
-                    ccc.push(task.getTookDateTime());
+                    ccc.push(getTookDateTime(task.time.doing, task.time.done));
                 }
 
                 results.push(ccc.join(', '));
@@ -105,12 +95,14 @@ const command = (commandStr) => {
             }, []).join(', ');
 
             console.log(targetTasks + '\n');
+
+            return tasks;
         }
     }
 
     const [method, param1, param2] = commandStr.replace(/\s/g, '').toLowerCase().split('$');
 
-    fns[method](param1, param2);
+    tasks = fns[method](tasks, param1, param2);
 };
 
 
@@ -127,7 +119,8 @@ command("update$1$done");
 command("update$2$done");
     
 // 걸린 시간 테스트를 위한 1일 1시간 30분 27초+
-tasks[0].time.done = new Date(new Date().getTime() +(1000 * 27) + (1000 * 60 * 90) + (1000 * 60 * 60 * 24));
+tasks[0].time.done = new Date(new Date().getTime() + (1000 * 27) + (1000 * 60 * 90) + (1000 * 60 * 60 * 24));
+
 // 27분 10초
 tasks[1].time.done = new Date(new Date().getTime() + (1000 * 10) + (1000 * 60 * 27));
 
